f2c73a8d65131554365fe9f873d66cdcddfb73c1
mincTeja <himateja135@gmail.com>
2021-02-22 21:44:34 +0530

    added client side js script

---
+++ public/static/shortUrl.js
@@ -0,0 +1,18 @@
+window.addEventListener('load',()=>{
+    const input = document.querySelector(".input");
+    const output = document.querySelector(".output");
+    const shortUrl = document.querySelector(".shortUrl");
+    const copy = document.querySelector(".copy");
+
+    shortUrl.addEventListener('click',()=>{
+        const urlPath="";
+        const options = {"long_url":input.value};
+        const request = new Request(urlPath+"urls",body:`'${options}'`);
+        fetch(request)
+        .then(res => res.json())
+        .then(id =>{
+            urlPath = urlPath + id;
+            output.innerHTML=urlPath;
+        });
+    });
+});
