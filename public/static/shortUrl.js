cf687dbf755570b00674770a631b3838aff72669
mincTeja <himateja135@gmail.com>
2021-02-22 23:14:55 +0530

    json stringify

--- public/static/shortUrl.js
+++ public/static/shortUrl.js
@@ -7,13 +7,14 @@ window.addEventListener('load',()=>{
     shortUrl.addEventListener('click',()=>{
         const urlPath="https://url-shortner-himateja.herokuapp.com/";
         const options = {"long_url":input.value};
+        console.log(JSON.stringify(options));
         console.log(options);
         const request = new Request(urlPath+"urls",{
             method: 'POST',
             headers: new Headers({
                 'Content-Type': 'application/json'
             }),
-            body:`'${options}'`
+            body:JSON.stringify(options)
         });
         fetch(request)
         .then(res => res.json())
