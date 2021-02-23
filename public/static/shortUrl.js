8386a9377b2a22cb749de340e6fa1e318a41563f
mincTeja <himateja135@gmail.com>
2021-02-23 08:08:53 +0530

    debugging

--- public/static/shortUrl.js
+++ public/static/shortUrl.js
@@ -6,19 +6,17 @@ window.addEventListener('load',()=>{
 
     shortUrl.addEventListener('click',()=>{
         const urlPath="https://url-shortner-himateja.herokuapp.com/";
-        
-        const options = {"long_url":input.value};
-        console.log(JSON.stringify(options));
-        console.log(options);
-        const request = new Request(urlPath+"urls",{
+        const options = {
             method: 'POST',
             headers: new Headers({
                 'Content-Type': 'application/json',
                 'Accept': 'application/json'
-
             }),
-            body:JSON.stringify(options)
-        });
+            body:JSON.stringify({"long_url":input.value})
+        }
+        console.log(JSON.stringify(options));
+        console.log(options);
+        const request = new Request('https://url-shortner-himateja.herokuapp.com/urls',options);
         fetch(request)
         .then(res => res.json())
         .then(data =>{
